afd24d918740fec3a07711b929d22103165e404e
Alireza <alirzahosinopoor.1390@gmail.com>
2025-09-17 22:37:47 +0330

    changed database to sqlite

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'twitter_clone',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'twitter_clone',
     }
 }
 
