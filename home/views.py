9a7ff062f897401b128728954cbc8160dd720bde
Alireza <alirzahosinopoor.1390@gmail.com>
2025-09-17 23:21:28 +0330

    fix feed issue

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files import File
 from django.contrib.auth.models import User
+from django.db.models import Count
 
 # My Module Imports
 from authentication.models import BasicUserProfile, Follower
@@ -43,12 +44,10 @@ def index(request):
 def home(request, page):
     """this is the homepage of the site and is an proxy redirector"""
 
-    # admin user session pop
-    # admin user session pop
-    # Deleting any sessions regarding top-tier type of users
-
     # Get the current users
     current_basic_user = get_current_user(request, User, ObjectDoesNotExist)
+    if current_basic_user is None:
+        return HttpResponseRedirect("/auth/signup/")
 
     current_basic_user_profile = get_current_user_profile(
         request,
@@ -57,149 +56,181 @@ def home(request, page):
         ObjectDoesNotExist
     )
 
-    # Tweet form processing
-    left_nav_tweet_form_processing(request, Tweet, current_basic_user_profile)
-
-    # Mobile Tweet Form processing
-    mobile_tweet_form_processing(request, Tweet, current_basic_user_profile)
-
-    # Topics to follow
-    topics_to_follow = get_topics_to_follow(Topic, ObjectDoesNotExist, random)
-
-    # Search form processing
-    if request.POST.get("right_nav_search_submit_btn"):
-        search_input = request.POST.get("search_input")
-        return HttpResponseRedirect("/search/" + str(search_input) + "/")
-
-    # Who to follow box cells
-    who_to_follow = get_who_to_follow(
-        BasicUserProfile, ObjectDoesNotExist, random
-    )
-
-    if request.POST.get("base_who_to_follow_submit_btn"):
-        hidden_user_id = request.POST.get("hidden_user_id")
-        followed_user = BasicUserProfile.objects.get(id=hidden_user_id)
-
-        # check if the followed user is in followings of current user
-        all_followings = Follower.objects.filter(
-            follower=current_basic_user_profile
-        )
-
-        for following in all_followings:
-            if following.following == followed_user:
-                pass
-            else:
-                new_follow = Follower(
-                    following=followed_user, follower=current_basic_user_profile,
+    # Handle tweet forms from different sections
+    if request.method == 'POST':
+        # Home page tweet form
+        if request.POST.get("home_page_tweet_form_submit_btn"):
+            tweet_content = request.POST.get("tweet_content")
+            tweet_image = request.FILES.get("tweet_image")
+            
+            if tweet_content or tweet_image:
+                new_tweet = Tweet(
+                    user=current_basic_user_profile, 
+                    content=tweet_content,
+                    image=tweet_image
                 )
-                new_follow.save()
+                new_tweet.save()
+                return HttpResponseRedirect("/home/0/")
+        
+        # Left nav tweet form
+        elif request.POST.get("hidden_panel_tweet_submit_btn"):
+            tweet_content = request.POST.get("tweet_content")
+            tweet_image = request.FILES.get("tweet_image")
+            
+            if tweet_content or tweet_image:
+                new_tweet = Tweet(
+                    user=current_basic_user_profile, 
+                    content=tweet_content,
+                    image=tweet_image
+                )
+                new_tweet.save()
+                return HttpResponseRedirect("/home/0/")
+        
+        # Mobile tweet form
+        elif request.POST.get("mobile_hidden_tweet_submit_btn"):
+            tweet_content = request.POST.get("tweet_content")
+            tweet_image = request.FILES.get("tweet_image")
+            
+            if tweet_content or tweet_image:
+                new_tweet = Tweet(
+                    user=current_basic_user_profile, 
+                    content=tweet_content,
+                    image=tweet_image
+                )
+                new_tweet.save()
+                return HttpResponseRedirect("/home/0/")
+        
+        # Search form
+        elif request.POST.get("right_nav_search_submit_btn"):
+            search_input = request.POST.get("search_input")
+            if search_input:
+                return HttpResponseRedirect("/search/" + str(search_input) + "/")
+        
+        # Follow user
+        elif request.POST.get("base_who_to_follow_submit_btn"):
+            hidden_user_id = request.POST.get("hidden_user_id")
+            try:
+                followed_user = BasicUserProfile.objects.get(id=hidden_user_id)
+                
+                # Check if already following
+                if not Follower.objects.filter(
+                    follower=current_basic_user_profile,
+                    following=followed_user
+                ).exists():
+                    new_follow = Follower(
+                        following=followed_user, 
+                        follower=current_basic_user_profile,
+                    )
+                    new_follow.save()
+                
+                return HttpResponseRedirect("/home/0/")
+            except ObjectDoesNotExist:
+                pass
+        
+        # Like tweet
+        elif request.POST.get("tweet_cell_like_submit_btn"):
+            current_tweet_id = request.POST.get("hidden_tweet_id")
+            try:
+                current_tweet = Tweet.objects.get(id=current_tweet_id)
+                
+                # Check if already liked
+                if not TweetLike.objects.filter(
+                    tweet=current_tweet,
+                    liker=current_basic_user_profile
+                ).exists():
+                    new_like = TweetLike(
+                        tweet=current_tweet,
+                        liker=current_basic_user_profile
+                    )
+                    new_like.save()
+                    
+                    # Create notification if not liking own tweet
+                    if current_tweet.user != current_basic_user_profile:
+                        new_notification = NotificationLike(
+                            notified=current_tweet.user,
+                            notifier=current_basic_user_profile,
+                            tweet=current_tweet,
+                        )
+                        new_notification.save()
+                
+                return HttpResponseRedirect("/home/0/")
+            except ObjectDoesNotExist:
+                pass
+        
+        # View tweet comments
+        elif request.POST.get("tweet_cell_comment_submit_btn"):
+            current_tweet_id = request.POST.get("hidden_tweet_id")
+            return HttpResponseRedirect("/tweet/" + str(current_tweet_id) + "/")
 
-        return HttpResponseRedirect(
-            "/profile/" + followed_user.user.username + "/"
-        )
-
-    # ----- base values ending point  ---------
-
-    # Home Page Tweet Form Processing
-    if request.POST.get("home_page_tweet_form_submit_btn"):
-        tweet_content = request.POST.get("tweet_content")
-        tweet_image = request.FILES.get("tweet_image")
-
-        new_tweet = Tweet(
-            user=current_basic_user_profile, content=tweet_content,
-            image=tweet_image
-        )
-        new_tweet.save()
-
-        return HttpResponseRedirect("/")
-
-    # Getting all Followings
+    # Get followings and own profile for tweet feed
+    followings = Follower.objects.filter(follower=current_basic_user_profile).values_list('following', flat=True)
+    users_to_include = list(followings) + [current_basic_user_profile.id]
+    
+    # Pagination setup
     try:
-        all_followings = Follower.objects.filter(
-            follower=current_basic_user_profile
-        )
-    except ObjectDoesNotExist:
-        all_followings = None
-
-    # Get all the tweets of followings
-    # At every page there will be 45 entries so always multiply it by that and
-    # then reduce your objects
-    current_page = page
-    previous_page = page-1
-    next_page = page+1
-
-    post_records_starting_point = current_page * 46
-    post_records_ending_point = post_records_starting_point + 46
-
-    tweet_feed = []
-
-    try:
-        all_tweets = Tweet.objects.all().order_by("-id")
-    except ObjectDoesNotExist:
-        all_tweets = None
-
-    for tweet in all_tweets:
-        for following in all_followings:
-            if tweet.user == following.following:
-                tweet_feed.append(tweet)
-
-    tweet_feed = tweet_feed[post_records_starting_point:post_records_ending_point]
-
-
-    # Getting the comment amount for each tweet
-    tweet_comment_amounts = {}
+        current_page = int(page)
+    except (ValueError, TypeError):
+        current_page = 0
+        
+    tweets_per_page = 46
+    previous_page = max(0, current_page - 1)
+    next_page = current_page + 1
+    
+    # Get tweets from followed users and self
+    tweet_feed = Tweet.objects.filter(
+        user__id__in=users_to_include
+    ).order_by('-creation_date')[
+        current_page * tweets_per_page:(current_page + 1) * tweets_per_page
+    ]
+    
+    # Prefetch related data for efficiency
+    tweet_ids = [tweet.id for tweet in tweet_feed]
+    
+    # Get comment counts
+    comment_counts = TweetComment.objects.filter(
+        tweet__id__in=tweet_ids
+    ).values('tweet').annotate(count=Count('id'))
+    
+    comment_count_map = {item['tweet']: item['count'] for item in comment_counts}
+    
+    # Get like counts
+    like_counts = TweetLike.objects.filter(
+        tweet__id__in=tweet_ids
+    ).values('tweet').annotate(count=Count('id'))
+    
+    like_count_map = {item['tweet']: item['count'] for item in like_counts}
+    
+    # Add counts to tweets
     for tweet in tweet_feed:
-        tweet_comments = TweetComment.objects.filter(tweet=tweet)
-        amount = len(tweet_comments)
-        tweet_comment_amounts[tweet.id] = amount
-
-    # Getting the likes for each tweet
-    tweet_like_amounts = {}
-    for tweet in tweet_feed:
-        tweet_likes = TweetLike.objects.filter(tweet=tweet)
-        amount = len(tweet_likes)
-        tweet_like_amounts[tweet.id] = amount
-
-    # Comment Form Processing
-    if request.POST.get("tweet_cell_comment_submit_btn"):
-        current_tweet_id = request.POST.get("hidden_tweet_id")
-        current_tweet = Tweet.objects.get(id=current_tweet_id)
-        return HttpResponseRedirect("/tweet/" + str(current_tweet.id) + "/")
-
-    # Like Form Processing
-    if request.POST.get("tweet_cell_like_submit_btn"):
-        current_tweet_id = request.POST.get("hidden_tweet_id")
-        current_tweet = Tweet.objects.get(id=current_tweet_id)
-        new_like = TweetLike(
-            tweet=current_tweet,
-            liker=current_basic_user_profile
-        )
-        new_like.save()
-        new_notification = NotificationLike(
-            notified=current_tweet.user,
-            notifier=current_basic_user_profile,
-            tweet=current_tweet,
-        )
-        new_notification.save()
-        return HttpResponseRedirect("/tweet/" + str(current_tweet.id) + "/")
-
+        tweet.tweet_comment_amount = comment_count_map.get(tweet.id, 0)
+        tweet.tweet_like_amount = like_count_map.get(tweet.id, 0)
+    
+    # Get who to follow (exclude self and already followed)
+    already_following = Follower.objects.filter(
+        follower=current_basic_user_profile
+    ).values_list('following__id', flat=True)
+    
+    who_to_follow = BasicUserProfile.objects.exclude(
+        id__in=already_following
+    ).exclude(
+        id=current_basic_user_profile.id
+    ).order_by('?')[:5]  # Random 5 profiles
+    
+    # Get topics to follow
+    topics_to_follow = Topic.objects.order_by('?')[:5]  # Random 5 topics
+    
     data = {
         "current_basic_user": current_basic_user,
         "current_basic_user_profile": current_basic_user_profile,
         "who_to_follow": who_to_follow,
         "topics_to_follow": topics_to_follow,
         "tweet_feed": tweet_feed,
-        "tweet_comment_amounts": tweet_comment_amounts,
-        "tweet_like_amounts": tweet_like_amounts,
-        "current_page": page,
+        "current_page": current_page,
         "previous_page": previous_page,
         "next_page": next_page,
     }
-
-    if current_basic_user == None:
-        return HttpResponseRedirect("/auth/signup/")
-    else:
-        return render(request, "home/home.html", data)
+    
+    return render(request, "home/home.html", data)
 
 
 def tweet_single(request, tweet_id):
